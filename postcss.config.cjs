8f68e0e66496804f03c3266af3ca41872bac47cf
Deepesh Genani <deepeshgenani@gmail.com>
2025-02-14 19:40:56 +0530

    Revert "card fixes"
    This reverts commit a036b9e2f53b8d4e80ec0d70eb6d97a09b24e6eb.

--- postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-// postcss.config.js
-module.exports = {  // Use module.exports for compatibility
-    plugins: [
-        require('@tailwindcss/postcss'), // Use this instead of 'tailwindcss'
-        require('autoprefixer'), // Autoprefixer usually comes AFTER Tailwind
-    ],
-};
